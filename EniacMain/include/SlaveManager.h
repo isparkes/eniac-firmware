c316dbc186e31b4286e09e041b5e8747898d2c80
isparkes <ian.sparkes@gmail.com>
2022-04-23 11:06:10 +0200

    Fix slaveMode config GUI

--- EniacMain/include/SlaveManager.h
+++ EniacMain/include/SlaveManager.h
@@ -3,6 +3,22 @@
 #include <Arduino.h>
 #include "Globals.h"
 
+// --------------------------------- Protocol ------------------------------------
+// Digits Update: Same data packet regardless of mode!
+//   Byte 1 - Mode 
+//      #define I2C_SET_SLAVE_DATA     0x1A
+//      #define I2C_SET_SLAVE_DATE     0x1B
+//      #define I2C_SET_SLAVE_SECS     0x1C
+//      #define I2C_SET_SLAVE_BLANK    0x1D
+//   Byte 2 - Current Second 00 - 59 
+//   Byte 3 - Current DoM 00 - 31
+//   Byte 4 - Current Mon 01 - 12
+//
+// Backlights Update: TBD
+//
+//
+// -------------------------------------------------------------------------------
+
 #define SLAVE_MODULE_I2C_ADDRESS 105
 
 // Used for timing out on the slave
